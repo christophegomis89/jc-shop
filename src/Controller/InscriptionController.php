54b3e6f2953280582cadd0f7ac0c9ebb51446086
Jean Christophe Gomis <gomischris2019@gmail.com>
2021-02-15 11:34:40 +0100

    mon avant dernier commit

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -24,6 +24,8 @@ class InscriptionController extends AbstractController
      */
     public function index(Request $request, UserPasswordEncoderInterface $encoder): Response
     {
+        $notification = null;
+
         $user = new User();
         $form = $this->createForm(InscriptionType::class, $user);
 
@@ -32,20 +34,36 @@ class InscriptionController extends AbstractController
         if($form->isSubmitted()&& $form->isValid()){
 
            $user = $form->getData();
+        //    $search_email= $this->em->getRepository( User::class)->findOneByEmail($user->getEmail());
 
-           $password = $encoder->encodePassword($user,$user->getPassword());
-            
-           $user->setPassword($password);
+        //    if(!$search_email){
+            $password = $encoder->encodePassword($user,$user->getPassword());
+            $user->setPassword($password);
            
-           $this->em->persist($user);
-           $this->em->flush();
+            $this->em->persist($user);
+            $this->em->flush();
+
+            // $mail = new Mail();
+            // $content = 'Bonjour'.'$user->getPrenom().'</br>Bienvenu dans KinkiHair'</br>
+            // $mail->send(user->getEmail),user->getPrenom(), 'Bienvenue dans Kinkihair'
+ 
+            $notification = 'Votre inscription a bien été pris en compte vous pouvez dés à présent vous connecter à votre compte';
+        //   }
+                      
+        //    else{
+          
+        //     $notification = 'L\'email que vous avez renseigné existe déjà !';
+        // }
+           
              return $this->redirectToRoute('gomis');     
         }
         
         
         return $this->render('inscription/index.html.twig',[
 
-            'form'=>$form->createView()
+            'form'=>$form->createView(),
+
+            'notification'=>$notification
 
         ]);
     }
